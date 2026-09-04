01d2dc0e095b51b583427fa1c6a58278b5f04719
xrn11 <mr1738866@gmail.com>
2026-09-03 21:54:26 -0700

    Operacion factorial

--- Aritmetica.java
+++ Aritmetica.java
@@ -7,4 +7,13 @@ public class Aritmetica {
     public int restar(int a, int b) {
         return a - b;
     }
+
+//Ramiro Mamani Ramos
+    public static long calcular(int numero) {
+        long resultado = 1;
+        for (int i = 1; i <= numero; i++) {
+            resultado = resultado * i;
+        }
+        return resultado;
+    }
 }
