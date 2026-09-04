c0fd096e014052805977201c4ee4e21ed74724dd
StefanyMTCH <mishelticona42@gmail.com>
2026-09-04 07:48:45 -0400

    agregar función de fibonacci

--- Aritmetica.java
+++ Aritmetica.java
@@ -3,12 +3,21 @@ public class Aritmetica {
         return a + b;
     }
 
-//Nombre: Melani Vanesa Ureña Vizcarra
+    // Nombre: Melani Vanesa Ureña Vizcarra
     public int restar(int a, int b) {
         return a - b;
     }
-    //Nombre: Franz Orellana
+
+    // Nombre: Franz Orellana
     public int multiplicar(int a, int b) {
         return a * b;
+    }
+
+    // Nombre: Mishel Stefani Ticona Chura
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
